04784feddc79f2c7d0c58f5460693d91d43ea06c
Baba website <63815531+mebri@users.noreply.github.com>
2026-09-12 21:59:01 +0300

    Update base path in Vite config
    Change base path for deployment to Cloudflare.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/marketplace-react/",
-});
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',  // This must be a single slash for Cloudflare!
+})
